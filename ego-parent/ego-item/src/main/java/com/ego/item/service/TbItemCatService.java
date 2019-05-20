68fadf59ae689814b43d787b78f432716ae09b06
ohxiaoguang <975129758@qq.com>
2019-05-20 18:51:07 +0800

    进度4/14：使用jsonp完成首页导航菜单

---
+++ ego-parent/ego-item/src/main/java/com/ego/item/service/TbItemCatService.java
@@ -0,0 +1,12 @@
+package com.ego.item.service;
+
+import com.ego.item.pojo.PortalMenu;
+
+public interface TbItemCatService {
+	/**
+	 * 查询出所有分类类目  转换格式
+	 * @return
+	 */
+	PortalMenu showCatMenu();
+
+}
